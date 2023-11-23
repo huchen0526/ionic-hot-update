88e5e9f3c558a1761853fc26dc165448498ee9db
Kyle Hu （胡沉） <Kyle_C_Hu@manulife.com>
2023-11-23 14:23:11 +0800

    live-update

---
+++ hot-update/capacitor.config.ts
@@ -0,0 +1,20 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hot-update',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https',
+  },
+  plugins: {
+    LiveUpdates: {
+      appId: 'cec51269',
+      channel: 'development',
+      autoUpdateMethod: 'background',
+      maxVersions: 4,
+    },
+  },
+};
+
+export default config;
